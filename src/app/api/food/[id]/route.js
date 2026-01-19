d6d259fc83fca6da3e1bb37f22af629efba4cd8f
Provash <provash20cb@gmail.com>
2026-01-20 05:21:50 +0600

    Get,post, delete api

--- src/app/api/food/[id]/route.js
+++ src/app/api/food/[id]/route.js
@@ -1,10 +1,42 @@
-import { food } from "../../route";
+// import { food } from "../../route";  no need
+
+import { connect } from "@/app/lib/dbConnect";
+import { ObjectId } from "mongodb";
+
+const foodCollection = connect("foods");
 
 export async function GET(request, {params}) {
    const {id} = await params;
 
-   const singleFood = food.find(fd=> fd.id==id) || {};
+   if(id.length!=24){
+    return Response.json({
+        status:400,
+        message:"send correct_id"
+    })
+   }
+
+   const query={_id:new ObjectId(id)}
+   const result=await foodCollection.findOne(query);
+
+//    const singleFood = food.find(fd=> fd.id==id) || {}; no need
 
 //    return Response.json({id})
-   return Response.json({singleFood})
+//    return Response.json({singleFood})
+   return Response.json(result);
+}
+
+export async function DELETE(request, {params}) {
+   const {id} = await params;
+
+   if(id.length!=24){
+    return Response.json({
+        status:400,
+        message:"send correct_id"
+    })
+   }
+
+   const query={_id:new ObjectId(id)}
+   const result=await foodCollection.deleteOne(query);
+
+   return Response.json(result);
 }
--- src/app/api/food/route.js
+++ src/app/api/food/route.js
@@ -1,7 +1,14 @@
-import { food } from "../route";
+import { connect } from "@/app/lib/dbConnect";
+// import { food } from "../route";
+const foodCollection = connect("foods");  //for all
 
 export async function GET(request) {
-    return Response.json({food})
+
+    // const foodCollection = connect("foods");
+    const result = await foodCollection.find().toArray();
+
+    // return Response.json({food})
+    return Response.json(result);
 }
 
 export async function POST(request) {
@@ -15,13 +22,17 @@ export async function POST(request) {
         })
     }
 
-    const newFood ={message, id:food.length + 1};
-    food.push(newFood);
+    // const newFood ={message, id:food.length + 1};
+    const newFood ={message, date: new Date().toISOString()};
+    // food.push(newFood); no need
 
-    return Response.json({
-        // status:200,
-        // data
-        acknowledged: true,
-        insertedId: newFood.id,
-    })
+    const result = await foodCollection.insertOne(newFood);
+
+    return Response.json(result)
+    // return Response.json({
+    //     // status:200,
+    //     // data
+    //     acknowledged: true,
+    //     insertedId: newFood.id,
+    // })
 }
